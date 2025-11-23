d480e41e43a2c625c9eab3bdbc7a2fda70ab6bb6
Leonardo Sala <s310598@studenti.polito.it>
2025-11-23 17:57:48 +0100

    eliminating semer column

--- main.py
+++ main.py
@@ -12,24 +12,22 @@ column_names = [
     'Nicotine', 'Semer', 'VSA'
 ]
 
-data_frame = pd.read_csv(file_path, names=column_names, header=None)
-
-# Semeron is a fictitious drug used to identify over-claimers
-# We remove all instances where != CL0
-data_frame = data_frame[data_frame['Semer'] == 'CL0']
-
-print(data_frame)
-
-# Crack small dataset
-# Cocaine, cannabis, LSD larger datasets
-
 drug_names = [
     'Alcohol', 'Amphet', 'Amyl', 'Benzos', 'Caff', 'Cannabis', 'Choc', 'Coke', 'Crack',
     'Ecstasy', 'Heroin', 'Ketamine', 'Legalh', 'LSD', 'Meth', 'Mushrooms',
     'Nicotine', 'Semer', 'VSA'
 ]
 
+data_frame = pd.read_csv(file_path, names=column_names, header=None)
+
+# Semeron is a fictitious drug used to identify over-claimers
+# We remove all instances where != CL0 and then remove the column
+data_frame = data_frame[data_frame['Semer'] == 'CL0']
+data_frame.drop('Semer', axis=1, inplace=True)
+
+# We simplify the dataframe into USER (1) and NON-USER (0)
 for drug in drug_names:
     data_frame[drug] = data_frame[drug].apply(lambda x: 1 if x in ['CL3','CL4','CL5','CL6'] else 0)
 
-print(data_frame)
+# Crack small dataset
+# Cocaine, cannabis, LSD larger datasets
