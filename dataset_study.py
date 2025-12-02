fced8d78d41eb56308e6683e25190a0f2fb6e82a
Matteo-Burzio <s307537@studenti.polito.it>
2025-12-02 16:28:44 +0100

    Separated in various files

---
+++ dataset_study.py
@@ -0,0 +1,33 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def import_dataset(file_path, data_frame):
+    
+    column_names = [
+        'ID', 'Age', 'Gender', 'Education', 'Country', 'Ethnicity',
+        'Nscore', 'Escore', 'Oscore', 'Ascore', 'Cscore', 'Impulsive', 'SS',
+        'Alcohol', 'Amphet', 'Amyl', 'Benzos', 'Caffeine', 'Cannabis', 'Choc', 'Coke', 'Crack',
+        'Ecstasy', 'Heroin', 'Ketamine', 'Legalh', 'LSD', 'Meth', 'Mushrooms',
+        'Nicotine', 'Semeron', 'VSA'
+    ]
+
+    drug_names = [
+        'Alcohol', 'Amphet', 'Amyl', 'Benzos', 'Caffeine', 'Cannabis', 'Choc', 'Coke', 'Crack',
+        'Ecstasy', 'Heroin', 'Ketamine', 'Legalh', 'LSD', 'Meth', 'Mushrooms',
+        'Nicotine', 'Semeron', 'VSA'
+    ]
+
+    data_frame = pd.read_csv(file_path, names=column_names, header=None)
+
+    # Semeron is a fictitious drug used to identify over-claimers
+    # We remove all instances where Semeron != CL0 and then remove the column
+    data_frame = data_frame[data_frame['Semeron'] == 'CL0']
+    data_frame.drop('Semeron', axis=1, inplace=True)
+    drug_names.remove('Semeron')
+
+    # We simplify the dataframe into USER (1) and NON-USER (0)
+    for drug in drug_names:
+        data_frame[drug] = data_frame[drug].apply(lambda x: 1 if x in ['CL3','CL4','CL5','CL6'] else 0)
+
--- main.py
+++ main.py
@@ -5,33 +5,18 @@ from sklearn.model_selection import train_test_split, StratifiedKFold
 from sklearn.tree import DecisionTreeClassifier, plot_tree
 from sklearn.metrics import confusion_matrix, accuracy_score
 
+from dataset_study import import_dataset
+
 file_path = 'drug_consumption.data'
+data_frame = []
 
-column_names = [
-    'ID', 'Age', 'Gender', 'Education', 'Country', 'Ethnicity',
-    'Nscore', 'Escore', 'Oscore', 'Ascore', 'Cscore', 'Impulsive', 'SS',
-    'Alcohol', 'Amphet', 'Amyl', 'Benzos', 'Caffeine', 'Cannabis', 'Choc', 'Coke', 'Crack',
-    'Ecstasy', 'Heroin', 'Ketamine', 'Legalh', 'LSD', 'Meth', 'Mushrooms',
-    'Nicotine', 'Semeron', 'VSA'
-]
+import_dataset(file_path, data_frame)
 
-drug_names = [
-    'Alcohol', 'Amphet', 'Amyl', 'Benzos', 'Caffeine', 'Cannabis', 'Choc', 'Coke', 'Crack',
-    'Ecstasy', 'Heroin', 'Ketamine', 'Legalh', 'LSD', 'Meth', 'Mushrooms',
-    'Nicotine', 'Semeron', 'VSA'
-]
+print(data_frame)
 
-data_frame = pd.read_csv(file_path, names=column_names, header=None)
 
-# Semeron is a fictitious drug used to identify over-claimers
-# We remove all instances where Semeron != CL0 and then remove the column
-data_frame = data_frame[data_frame['Semeron'] == 'CL0']
-data_frame.drop('Semeron', axis=1, inplace=True)
-drug_names.remove('Semeron')
 
-# We simplify the dataframe into USER (1) and NON-USER (0)
-for drug in drug_names:
-    data_frame[drug] = data_frame[drug].apply(lambda x: 1 if x in ['CL3','CL4','CL5','CL6'] else 0)
+
 
 # Crack small dataset
 # Cocaine, cannabis, LSD larger datasets
@@ -101,7 +86,7 @@ for f in range(len(feature_columns)):
 plt.figure(figsize=(20, 10))
 plot_tree(decision_tree, feature_names=feature_columns, class_names=['Non-User', 'User'], filled=True, fontsize=10)
 plt.title("Decision Tree")
-plt.savefig('decision_tree.png')
+plt.savefig('Pictures\\decision_tree.png')
 
 print("_" * 46)
 
@@ -166,4 +151,4 @@ print(f"Mean Specificity: {np.mean(specificities):.4f}")
 plt.figure(figsize=(20, 10))
 plot_tree(decision_tree, feature_names=feature_columns, class_names=['Non-User', 'User'], filled=True, fontsize=10)
 plt.title(f"Decision Tree for {target_column} (Fold {fold_num-1})")
-plt.savefig('decision_tree_fold5.png') # Saves the file so it doesn't block execution
+plt.savefig('Pictures\\decision_tree_fold5.png') # Saves the file so it doesn't block execution
