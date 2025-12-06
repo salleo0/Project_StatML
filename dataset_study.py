1ff2896d10cedcf7ec2af56b939b81887c58d393
Federica Belardinelli <federica.belardinelli@epfl.ch>
2025-12-06 20:33:37 +0100

    Plots describing the preliminary choices

--- dataset_study.py
+++ dataset_study.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import os
 
 def import_dataset(file_path):
     
@@ -36,11 +36,61 @@ def import_dataset(file_path):
 
     return data_frame
 
-#Things to explain (mainly about the dataset) and the changes made on it:
+# Things to explain (mainly about the dataset) and the changes made on it:
 #1. Goal of the main study based on this dataset 
 #2. Input and output varibles
 #3. The role of the output variable 'Semeron' and how it has been treated in the dataset we created
 #4. The simplification of the categories of the output variables (the various drugs): the initial categories were splitted in two groups
 #   and the problem became a classification problem
-#5. The choice of one specific/more specific (discuss about this) output variables. Why it has been made that particular choice?
+#5. The drop of the input variables 'Ethnicity' and 'Country'
+#6. The choice of one specific/more specific (discuss about this) output variables. Why it has been made that particular choice?
 #   The exploratory analysis could help to answer this question.
+
+def feat_barplot(data_frame, x):
+    # Relative frequencies for the input feature x
+    freq = data_frame[x].value_counts(normalize=True)
+
+    # Barplot with relative frequencies
+    plt.figure(figsize=(8, 3))
+    freq.plot(kind='bar')
+    plt.xticks(rotation=90)
+    plt.ylabel('Relative frequency')
+    plt.title(f'Relative frequencies for {x}')
+    plt.tight_layout()
+
+    # Saving the plot
+    filename = f'{x}_relative_barplot.png'
+    folder = 'dataset_pictures'
+    filepath = os.path.join(folder, filename)
+    plt.savefig(os.path.join(folder, filename))
+    plt.close()
+
+    # Returning the relative frequencies in case I have to watch at the specific values
+    return freq
+
+
+def proportions_plot(data_frame, y):
+    # Relative frequencies (0 and 1) for each drug
+    users_per_drug_rel = {
+        drug: data_frame[drug].value_counts(normalize=True).sort_index()
+        for drug in y
+    }
+
+    users_rel_df = pd.DataFrame(users_per_drug_rel).fillna(0.0)
+
+    # Barplot with the relative frequencies for each drug
+    fig, ax = plt.subplots(figsize=(15, 8))
+    users_rel_df.plot(kind='bar', width=0.8, ax=ax)
+    ax.set_title('Relative frequency of users per drug type')
+    ax.set_xlabel('User (1) and Non-User (0)')
+    ax.set_ylabel('Proportion of people')
+    ax.legend(title='Drugs', bbox_to_anchor=(1.05, 1), loc='upper left')
+    ax.set_ylim(0, 1)
+    fig.tight_layout()
+
+    # Saving the plot
+    plt.savefig('dataset_pictures/relative_users_per_drug.png')
+    plt.close(fig)
+
+    # Returning the relative frequencies for each drug in case of need
+    return users_rel_df
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from dataset_study import import_dataset
+from dataset_study import import_dataset, feat_barplot, proportions_plot
 from decision_trees import naive_tree, cv_tree
 from shap_predictions import shap_estimation
 
@@ -11,12 +11,27 @@ data_frame = import_dataset(file_path)
 # Select features and target drugs
 # Note that Country and Ethnicity are not considered
 feature_columns = ['Age', 'Gender', 'Education', 
-                'Nscore', 'Escore', 'Oscore', 'Cscore', 'Ascore', 'Impulsive', 'SS']
+                   'Nscore', 'Escore', 'Oscore', 'Cscore', 'Ascore', 'Impulsive', 'SS']
 target_column = 'LSD'
 
-# Extract features and target
+# Output features 
+output_columns = ['Alcohol', 'Amphet', 'Amyl', 'Benzos', 'Caffeine', 'Cannabis',
+                  'Choc', 'Coke', 'Crack', 'Ecstasy', 'Heroin', 'Ketamine',
+                  'Legalh', 'LSD', 'Meth', 'Mushrooms', 'Nicotine', 'VSA']
+
+# Extract input features, output features and target
 X = data_frame[feature_columns].copy()
+Y = data_frame[output_columns].copy()
 y = data_frame[target_column].copy()
+
+# Relative barplot of the input feature 'Country'
+feat_barplot(data_frame, 'Country')
+
+#Relative barplot of the input feature 'Ethnicity'
+feat_barplot(data_frame, 'Ethnicity')
+
+# Relative barplot for all the drugs
+proportions_plot(data_frame, Y)
 
 # Naive tree
 naive_tree(X, y, target_column, feature_columns)
